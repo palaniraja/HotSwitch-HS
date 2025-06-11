1f705c95ea46581e79c5f66fc337b43393e05a40
palaniraja <palaniraja@live.com>
2025-06-11 08:19:28 -0700

    customized font & color

--- lib/common/CanvasConstants.lua
+++ lib/common/CanvasConstants.lua
@@ -1,19 +1,19 @@
 local CanvasConstants = {
-    FONT_SIZE = 17,
+    FONT_SIZE = 18,
 
     PANEL_W = 520,
 
-    PADDING = 10,
+    PADDING = 15,
     KEY_W = 24,
-    KEY_LEFT_PADDING = 10,
-    ROW_HEIGHT = 26,
+    KEY_LEFT_PADDING = 15,
+    ROW_HEIGHT = 28,
 
-    APP_ICON_W = 26,
+    APP_ICON_W = 28,
     APP_NAME_W = 160,
 
     OUTLINE_RECTANGLE_ALPHA = 0.5,
-    INLINE_RECTANGLE_ALPHA = 0.7,
-    SELECTED_ROW_ALPHA = 0.2,
+    INLINE_RECTANGLE_ALPHA = 0.8,
+    SELECTED_ROW_ALPHA = 0.3,
     TEXT_ALPHA = 1,
 
     TEXT_WHITE_VALUE = 0.92,
@@ -21,7 +21,22 @@ local CanvasConstants = {
     TOAST_W = 520,
     TOAST_H = 46,
     TOAST_ALPHA = 0.8,
-    TOAST_FONT_SIZE = 20,
+    TOAST_FONT_SIZE = 22,
+
+    KEY_FONT_NAME = "JetBrains Mono",
+    KEY_FONT_SIZE = 16,
+    KEY_FONT_COLOR_AUTO_GEN = {
+        alpha = 1,
+        blue = 0.1,
+        green = 0.9,
+        red = 0.9
+    },
+    KEY_FONT_COLOR_CUSTOM_SET = {
+        alpha = 1,
+        blue = 0.92,
+        green = 0.92,
+        red = 0.92
+    },
 }
 
 -- for debug
